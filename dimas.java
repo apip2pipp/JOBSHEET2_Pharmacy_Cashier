29bdf0501e85943a8a1cd35138b0041eba5c0e70
Samidkun <samiddasproa@gmail.com>
2023-10-07 22:16:25 +0700

    aku bingung

---
+++ dimas.java
@@ -0,0 +1,12 @@
+/**
+ * isengmendo
+ */
+public class isengmendo {
+
+    public static void main(String[] args) {
+        System.out.println("Saya mendo banget");
+        System.out.println("Dan saya bangga");
+    
+        
+    }
+}
