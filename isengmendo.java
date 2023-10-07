c3c646aa3b53ca8d51a986070232446cfcb10cd1
Samidkun <samiddasproa@gmail.com>
2023-10-07 22:09:43 +0700

    info

---
+++ isengmendo.java
@@ -0,0 +1,12 @@
+/**
+ * isengmendo
+ */
+public class isengmendo {
+
+    public static void main(String[] args) {
+        System.out.println("Saya mendo");
+        System.out.println("Dan saya bangga");
+    
+        
+    }
+}
